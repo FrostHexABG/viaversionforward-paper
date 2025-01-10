ed327e7a2ba1bab28d2ac17f4031b8003ac937d5
JustBru00 <justbru00@gmail.com>
2025-01-09 19:06:16 -0500

    First Commit

---
+++ src/main/java/com/frosthex/viaversionforwardpaper/ViaVersionForwardPaperPlugin.java
@@ -0,0 +1,17 @@
+package com.frosthex.viaversionforwardpaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ViaVersionForwardPaperPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
